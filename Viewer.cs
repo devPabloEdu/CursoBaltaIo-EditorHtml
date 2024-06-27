0976e308788ea2b326d02d663b71844a30403157
Devpabloedu <158394108+devPabloEdu@users.noreply.github.com>
2024-06-27 19:41:48 -0300

    projeto concluido

--- Viewer.cs
+++ Viewer.cs
@@ -1,15 +1,18 @@
 using System;
 using System.Text.RegularExpressions;
+
 namespace EditorHtml
 {
-    public class Viewer{
-        public static void Show(string text){
+    public class Viewer
+    {
+        public static void Show(string text)
+        {
             Console.Clear();
             Console.BackgroundColor = ConsoleColor.White;
             Console.ForegroundColor = ConsoleColor.Black;
             Console.Clear();
-            Console.WriteLine("MODO DE VISUALIZAÇÃO");
-            Console.WriteLine("----------------------");
+            Console.WriteLine("MODO VISUALIZAÇÃO");
+            Console.WriteLine("----------------------------------------");
             Replace(text);
             Console.WriteLine("----------------------------------------");
             Console.WriteLine("Pressione qualquer tecla para voltar ao menu");
@@ -17,24 +20,25 @@ namespace EditorHtml
             Menu.Show();
         }
 
-        public static void Replace(string text){
-            var strong = new Regex(@"<\s*strong[^>]*.(.*?),\s*/\s*strong>");
+        public static void Replace(string text)
+        {
+            var strong = new Regex(@"<\s*strong[^>]*>(.*?)<\s*/\s*strong>");
             var words = text.Split(' ');
-
             for (var i = 0; i < words.Length; i++)
             {
-                if(strong.IsMatch(words[i])){
+                if (strong.IsMatch(words[i]))
+                {
                     Console.ForegroundColor = ConsoleColor.Blue;
                     Console.Write(
                         words[i].Substring(
                             words[i].IndexOf('>') + 1,
-                            (
-                                (words[i].LastIndexOf('<') - 1) - words[i].IndexOf('>') 
-                                )
+                            ((words[i].LastIndexOf('<') - 1) - words[i].IndexOf('>'))
                         )
                     );
                     Console.Write(" ");
-                } else {
+                }
+                else
+                {
                     Console.ForegroundColor = ConsoleColor.Black;
                     Console.Write(words[i]);
                     Console.Write(" ");
