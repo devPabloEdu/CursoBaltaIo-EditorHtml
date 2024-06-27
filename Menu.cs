2609bf44fba41ebdf6a17012d089b58216132696
Devpabloedu <158394108+devPabloEdu@users.noreply.github.com>
2024-06-27 17:25:27 -0300

    criando a função drawScreen

--- Menu.cs
+++ Menu.cs
@@ -10,7 +10,14 @@ namespace EditorHtml
             Console.BackgroundColor = ConsoleColor.Blue;
             Console.ForegroundColor = ConsoleColor.Black;
 
-            
+
+        }
+
+        public static void DrawScreen(){
+            Console.Write("+");
+            for(int i = 0; i <= 30; i++){
+                Console.Write("+");
+            }
         }
 
 
