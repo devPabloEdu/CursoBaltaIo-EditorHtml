f265e0b5ddf7880fc899ceb0b93341914ed8207e
Devpabloedu <158394108+devPabloEdu@users.noreply.github.com>
2024-06-27 19:35:22 -0300

    bug fix

--- Menu.cs
+++ Menu.cs
@@ -63,7 +63,7 @@ namespace EditorHtml
             switch(option) {
                 case 1 : Editor.Show();
                 break;
-                case 2 : Console.WriteLine("View");
+                case 2 : Viewer.Show("");
                 break;
                 case 0 : {
                     Console.Clear();
