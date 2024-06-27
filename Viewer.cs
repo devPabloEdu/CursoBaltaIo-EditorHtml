a9827468ca2cfb07f0fe038316158d91b9163090
Devpabloedu <158394108+devPabloEdu@users.noreply.github.com>
2024-06-27 19:14:58 -0300

    criando o metodo replace e utilizando um regex de sstrong

---
+++ Viewer.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Text.RegularExpressions;
+namespace EditorHtml
+{
+    public class Viewer{
+        public static void Show(string text){
+            Console.Clear();
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.Clear();
+            Console.WriteLine("MODO DE VISUALIZAÇÃO");
+            Console.WriteLine("----------------------");
+            Replace(text);
+            Console.WriteLine("----------------------");
+            Console.ReadKey();
+            Menu.Show();
+        }
+
+        public static void Replace(string text){
+            var strong = new Regex(@"<\s*strong[^>]*.(.*?),\s*/\s*strong>");
+        }
+    }
+}
