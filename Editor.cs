31f75f062f5da7fc787ce4a6ff52962126d8db28
Devpabloedu <158394108+devPabloEdu@users.noreply.github.com>
2024-06-27 18:30:50 -0300

    bug fix no metodo fileSave

--- Editor.cs
+++ Editor.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Text;
+using System.IO;
+using Microsoft.VisualBasic;
 
 namespace EditorHtml
 {
@@ -21,9 +23,29 @@ namespace EditorHtml
                 file.Append(Console.ReadLine());
                 file.Append(Environment.NewLine);
             } while (Console.ReadKey().Key != ConsoleKey.Escape);
+
             Console.WriteLine("-------------");
             Console.WriteLine("Deseja salvar o arquivo?");
             Console.WriteLine("-------------");
+            var optionSave = Console.ReadLine();
+            if(optionSave == "sim")  
+            {
+                SaveFile(file);
+                
+            }
+                else {
+                Console.WriteLine("Ok, arquivo não esta salvo!");
+            }
+        }
+
+        public static void SaveFile(StringBuilder file){
+            Console.Clear();
+                Console.WriteLine("Qual caminho deseja salvar?");
+                var path = Console.ReadLine();
+                using (var newSavedFile = new StreamWriter(path))
+                {newSavedFile.Write(file);
+                }
+                Console.WriteLine("Ok, arquivo salvo!");
         }
     }
 }
--- Menu.cs
+++ Menu.cs
@@ -61,7 +61,7 @@ namespace EditorHtml
 
         public static void HandleMenuOption(short option){
             switch(option) {
-                case 1 : Console.WriteLine("EDITOR");
+                case 1 : Editor.Show();
                 break;
                 case 2 : Console.WriteLine("View");
                 break;
