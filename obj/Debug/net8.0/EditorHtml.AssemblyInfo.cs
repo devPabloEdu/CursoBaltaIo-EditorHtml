33a112f67d92bf8415880ef275385146658d1105
Devpabloedu <158394108+devPabloEdu@users.noreply.github.com>
2024-06-27 23:38:48 -0300

    .

--- obj/Debug/net8.0/EditorHtml.AssemblyInfo.cs
+++ obj/Debug/net8.0/EditorHtml.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EditorHtml")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4badeff60b9409d06c2f729e85f6c3de22cc9789")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4762207aa1b52a6035392ae366090227431ffb10")]
 [assembly: System.Reflection.AssemblyProductAttribute("EditorHtml")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EditorHtml")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
